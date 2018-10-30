fe66222f0cfb0d5d540c2fd7e014b790baecb707
Eliana Florian <eliflorian_06@hotmail.com>
2018-10-30 02:32:49 -0300

    Verifica que la matriz de coeficientes no esté vacía y que sea diagonalmente dominante (Sólo con números positivos)

--- SIEL/WebContent/index.js
+++ SIEL/WebContent/index.js
@@ -1,36 +1,109 @@
 $(document).ready(function(){
 	
-//Mensaje de filas y columnas ingresados y evento click
+//Oculto botón verificar
+	
+	$("#verificar").hide();
+	
+//Función para crear tabla A
+	
+	function crear_tabla(filas, columnas, idTabla, readOnly){
+		
+		var tabla = "";
+		
+		for(var i=0; i<filas; i++){
+			
+			tabla += "<tr id='fila_'"+i+"''>";
+			
+			for(var j=0; j<columnas; j++){
+				
+				tabla+="<td id='col_'"+j+"'>";
+				tabla+="<input type='number' id='"+(i)+(j)+"'";
+				if(readOnly){
+					tabla+=" readonly";
+				}
+				tabla+= "/>";
+				tabla+="</td>";
+			}
+			
+			tabla+="</tr>";
+		}
+		
+		$("#"+idTabla).html(tabla);
+	};
+	
+//Función para ver que la matriz tenga todos los datos cargados
+	
+	function con_datos(){
+		
+		$("#tabla_A input").each(function(){
+			if($(this).val() == ''){
+				alert("Por favor, cargue todos los valores de la matriz");
+				//$(this).focus();
+				return false;
+			}
+				
+		});
+	}
+	
+//Me fijo que la matriz sea diagonalmente dominante
+	
+	function diagonalmente_dominante(){
+		
+		var filas=$("#filas").val();
+		var columnas=$("#columnas").val();
+		
+		for(var i=0;i<filas;i++){
+			
+			var acum=0;
+			var diagonal=0;
+			
+			for(var j=0;j<columnas;j++){
+				
+				
+				if(i==j){
+					
+					diagonal = parseInt($("#"+(i)+(j)).val());
+				}else{
+					
+					acum += parseInt($("#"+(i)+(j)).val());
+				}
+			}
+			
+			
+			if(acum>diagonal){
+				
+				alert("La matriz ingresada no es diagonalmente dominante");
+				return false;
+			}
+		}
+	}
+	
+//Click de Generar Tabla
 	
 	$("#generar").click(function(){
 		var filas=$("#filas").val();
 		var columnas=$("#columnas").val();
-		alert("Se va a generar una matriz de:\n"+filas+" filas \n"+columnas+" columnas");
-		crear_tabla(filas,columnas);
+		//alert("Se va a generar una matriz de:\n"+filas+" filas \n"+columnas+" columnas");
+		
+		if(filas=='' || columnas==''){
+			alert("Por favor, ingrese tanto filas como columnas");
+		}else{
+			crear_tabla(filas,columnas,"tabla_A",false);
+			//crear_tabla(columnas,1,"tabla_X",true);
+			//crear_tabla(filas,1,"tabla_B",false);
+			$("#verificar").show();
+		}
 	});
 	
-//Función para crear tabla
+//Click de Verificar
 	
-	function crear_tabla(filas, columnas){
-		
-		var table = "";
-		
-		for(var i=0; i<filas; i++){
+//Me fijo que la matriz esté completamente cargada y sea diagonalmente dominante
+	
+	$("#verificar").click(function(){
+		//con_datos();
+		diagonalmente_dominante();
+	})
 			
-			table += "<tr id='fila_'"+i+"''>";
-			
-			for(var j=0; j<columnas; j++){
-				
-				table+="<td id='col_'"+j+"'>";
-				table+="<input type='text' />";
-				table+="</td>";
-			}
-			
-			table+="</tr>";
-		}
-		
-		$("#tabla_A_body").html(table);
-	};
 	
 })
 
