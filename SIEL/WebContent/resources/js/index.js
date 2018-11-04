0af363f5fb9aa47562bc1f9abe0a0d147505d596
nalbarracin <nazareno.albarracin@grupoesfera.com.ar>
2018-11-04 03:38:56 -0300

    Cambia el orden de las funciones, colocando arriba las acciones

--- SIEL/WebContent/resources/js/index.js
+++ SIEL/WebContent/resources/js/index.js
@@ -9,6 +9,54 @@ $(document).ready(function () {
 
 	$("#verificar").hide();
 
+	//Click de Generar Tabla
+
+	$("#generar").click(function () {
+		filas = $("#filas_columnas").val();
+
+		if (filas == '') {
+			alert("Por favor, ingrese las filas y columnas");
+		} else {
+			dataA = generar_matriz(filas, filas);
+			matrizA = crear_grilla(idTablaA, dataA, false, true);
+			$("#verificar").show();
+		}
+	});
+
+	$("#verificar").click(function () {
+
+		if(!esCuadrada(matrizA)){
+			alert("La matriz ingresada debe ser una matriz cuadrada");
+			return;
+		}
+		if(tieneValoresVacios(matrizA)){
+			alert("Por favor, cargue todos los valores de la matriz");
+			return;
+		}
+		if(tieneValoresInvalidos(matrizA)){
+			alert("Por favor, corrija todos los valores invalidos");
+			return;
+		}
+		if(!esDiagonalmenteDominante(matrizA)){
+			alert("La matriz ingresada no es diagonalmente dominante");
+			return;
+		}
+
+		dataX = generar_matriz(matrizA.countRows(), 1);
+		//Si conozco que valores voy a cargar en la matriz, antes de crear la tabla, los seteo de esta forma
+		for (var i = 0; i < matrizA.countRows(); i++) {
+			dataX[i][0] = "X" + [i + 1];
+		}
+		matrizX = crear_grilla(idTablaX, dataX, true, false);
+
+		//en caso de tener que modificar los datos de una tabla ya renderizada, sobreescribo su data, y vuelvo a renderizar la matriz
+		//dataX[0][0] = "Prueba";
+		//matrizX.render();
+
+		dataB = generar_matriz(matrizA.countRows(), 1);
+		matrizB = crear_grilla(idTablaB, dataB, false, true);
+	})
+
 	function crear_grilla(idTabla, data, readOnly, numeros) {
 
 		var container = document.getElementById(idTabla);
@@ -65,16 +113,12 @@ $(document).ready(function () {
 	}
 
 	function esDiagonalmenteDominante(matriz) {
-
 		filas = matriz.countRows();
 
 		for (var i = 0; i < filas; i++) {
-
 			var acum = 0;
 			var diagonal = 0;
-
 			for (var j = 0; j < filas; j++) {
-
 				if (i == j) {
 					diagonal = parseInt(matriz.getDataAtCell(i, j));
 				} else {
@@ -87,54 +131,6 @@ $(document).ready(function () {
 		}
 		return true;
 	}
-
-	//Click de Generar Tabla
-
-	$("#generar").click(function () {
-		filas = $("#filas_columnas").val();
-
-		if (filas == '') {
-			alert("Por favor, ingrese las filas y columnas");
-		} else {
-			dataA = generar_matriz(filas, filas);
-			matrizA = crear_grilla(idTablaA, dataA, false, true);
-			$("#verificar").show();
-		}
-	});
-
-	$("#verificar").click(function () {
-
-		if(!esCuadrada(matrizA)){
-			alert("La matriz ingresada debe ser una matriz cuadrada");
-			return;
-		}
-		if(tieneValoresVacios(matrizA)){
-			alert("Por favor, cargue todos los valores de la matriz");
-			return;
-		}
-		if(tieneValoresInvalidos(matrizA)){
-			alert("Por favor, corrija todos los valores invalidos");
-			return;
-		}
-		if(!esDiagonalmenteDominante(matrizA)){
-			alert("La matriz ingresada no es diagonalmente dominante");
-			return;
-		}
-
-		dataX = generar_matriz(matrizA.countRows(), 1);
-		//Si conozco que valores voy a cargar en la matriz, antes de crear la tabla, los seteo de esta forma
-		for (var i = 0; i < matrizA.countRows(); i++) {
-			dataX[i][0] = "X" + [i + 1];
-		}
-		matrizX = crear_grilla(idTablaX, dataX, true, false);
-
-		//en caso de tener que modificar los datos de una tabla ya renderizada, sobreescribo su data, y vuelvo a renderizar la matriz
-		//dataX[0][0] = "Prueba";
-		//matrizX.render();
-
-		dataB = generar_matriz(matrizA.countRows(), 1);
-		matrizB = crear_grilla(idTablaB, dataB, false, true);
-	})
 })
 
 
