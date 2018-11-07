0aa977077e439f153360fbf4b27cc7072fdef8a6
nalbarracin <nazareno.albarracin@grupoesfera.com.ar>
2018-11-07 01:56:15 -0300

    Agrega elementos faltantes en la pantalla

--- SIEL/WebContent/resources/js/index.js
+++ SIEL/WebContent/resources/js/index.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
 
 	var filas,
-		matrizA, matrizX, matrizB,
-		dataA, dataB, dataC,
+		matrizA, matrizB, vectorInicial,
+		dataA, dataB, dataVectorInicial
 		idTablaA = "tabla_A",
 		idTablaX = "tabla_X",
-		idTablaB = "tabla_B";
+		idTablaB = "tabla_B",
+		idVectorInicial = "vector_inicial",
+		idCuadroVectorInicial = "cuadro_vector_inicial";
 
 	$("#verificar").hide();
+	$("#cuadro_vector_inicial").hide();
 
 	$("#generar").click(function () {
 		filas = $("#filas_columnas").val();
@@ -22,13 +25,15 @@ $(document).ready(function () {
 		$("#verificar").show();
 		$("#tabla_X").hide();
 		$("#tabla_B").hide();
+		$("#cuadro_vector_inicial").hide();
 	});
 
 	$("#verificar").click(function () {
 
-		if (!esValidaMatrizA) {
+		if (!esValidaMatrizA(matrizA)) {
 			$("#tabla_X").hide();
 			$("#tabla_B").hide();
+			$("#cuadro_vector_inicial").hide();
 			return;
 		}
 
@@ -36,19 +41,34 @@ $(document).ready(function () {
 
 		if ((typeof matrizB == "undefined") || !(isVisible)) {
 
-			dataX = generarData(matrizA.countRows(), 1);
+			var dataX = generarData(matrizA.countRows(), 1);
 
 			for (var i = 0; i < matrizA.countRows(); i++) {
 				dataX[i][0] = "X" + [i + 1];
 			}
-			matrizX = crearMatriz(idTablaX, dataX, true, false);
+			crearMatriz(idTablaX, dataX, true, false);
 
 			dataB = generarData(matrizA.countRows(), 1);
 			matrizB = crearMatriz(idTablaB, dataB, false, true);
+
+			dataVectorInicial = generarData(1, matrizA.countRows());
+			vectorInicial = crearMatriz(idVectorInicial, dataVectorInicial, false, true);
 		}
 
-		$("#verificar").hide();
+//		$("#verificar").hide();
 		$("#tabla_X").show();
 		$("#tabla_B").show();
+		$("#cuadro_vector_inicial").show();
 	})
+
+	$("#ejecutar").click(function () {
+		console.log('ejecutar');
+		console.error('Aun no implementado');
+		//TODO
+		//validar que las 3 matrices, el vector inicial,
+		//la cantidad de decimales, y la cota de error tengan datos
+		//invocar a la función de ejecución del algoritmo según el que esté seleccionado
+	})
+
+
 })
